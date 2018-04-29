41e285550832ab2eca7d4b3188a64125c04be085
七月 <foolishqi@126.com>
2018-04-30 03:31:38 +0800

    添加认证服务；

---
+++ src/Services/Identity/IdentityServer/Config.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace mCore.Services.IdentityServer
+{
+    public class Config
+    {
+        // ApiResources define the apis in your system
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("process", "Process Service")
+            };
+        }
+
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+            };
+        }
+
+        internal static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
